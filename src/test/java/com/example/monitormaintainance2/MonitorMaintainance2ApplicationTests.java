9e8936f00675ed59a9fb2b70fc516557732e1a89
Vansl1220 <wi22b110@technikum-wien.at>
2023-06-12 22:33:52 +0200

    Initial commit

---
+++ src/test/java/com/example/monitormaintainance2/MonitorMaintainance2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monitormaintainance2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorMaintainance2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
